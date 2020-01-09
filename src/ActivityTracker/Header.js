247a7c6f1e909c0fdb481cb602f0d8f52cc3a099
Hossayin <alihossein96@gmail.com>
2020-01-09 13:08:26 -0500

    worked with components

---
+++ src/ActivityTracker/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header>
+      {/* <img /> */}
+      <p>ActivityTrackerForYOU</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/ActivityTracker/Main.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+function Main() {}
+
+export default Main;
